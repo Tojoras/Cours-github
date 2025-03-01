1197f15f000695a3a988902ad79c8944ff475b93
Tojoras <tojomandimby@gmail.com>
2025-03-01 14:27:39 +0300

    Initial Commit

---
+++ Cours github/Cours_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cours_githubApp.swift
+//  Cours github
+//
+//  Created by Tojo Ras on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cours_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
