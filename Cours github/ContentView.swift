9060ac812ad83eaca9f84e4653e6b34139359059
Fiantso <nyainaantso@gmail.com>
2025-03-01 16:10:36 +0300

    test fi

--- Cours github/ContentView.swift
+++ Cours github/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("fffhhffjhfhghfhgfh")
         }
         .padding()
     }
